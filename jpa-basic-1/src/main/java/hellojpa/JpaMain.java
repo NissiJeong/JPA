75b492ecdc36c99319e95f09b4f8bdad0e3d6739
NissiJeong <jnissi92@gmail.com>
2024-09-19 13:45:50 +0900

    JPA에 대하여

--- jpa-basic-1/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic-1/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -43,6 +45,33 @@ public class JpaMain {
             //자바 collection을 다룬것 처럼 설계가 되어 있다.
             //JPA를 통해서 Entity를 가져오면 JPA가 관리, JPA가 변경 됐는지 안됐는지 트랜잭션 커밋한 시점에 확인. 변경이 되어있으면 update 쿼리 작성.
             findMember.setName("Hello JPA");
+
+            //jpql은 복잡한 쿼리, 통계 쿼리 등 직접 쿼리를 작성해서 쿼리 명령어를 실행시킬 수 있음. 
+            //jpql은 테이블 대상으로 코드를 짜지 않음. 멤버 객체를 대상으로 코드를 작성. Member 엔티티 객체를 대상으로 
+            //아래는 실제 실행 쿼리
+            // /* select
+            //     m 
+            // from
+            //     Member as m */ select
+            //     member0_.id as id1_0_,
+            //     member0_.name as name2_0_ 
+            // from
+            //     Member member0_
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            //jpql은 어떤 메리트가 있나?
+            //페이징을 하거나 setFirstResult, setMaxResult 등으로 하면 자동을 반영 됨. 
+            //jpql은 객체를 대상으로 하는 객체지향 쿼리, 내가 선택한 database에 맞춰서 알아서 변경을 해 줌.
+
+            /*
+             * 정리!
+             * 테이블이 아닌 객체를 대상으로 검색하는 객체지향 
+             * SQL을 추상화해서 특정 데이터 베이스 SQL에 의존하지 않음.
+             * JPQL을 한마디로 정의하면 객체지향 SQL
+             */
+
+            for(Member member : result ){
+                System.out.println("member: "+member.getName());
+            }
 
             tx.commit();
         }catch(Exception e){
