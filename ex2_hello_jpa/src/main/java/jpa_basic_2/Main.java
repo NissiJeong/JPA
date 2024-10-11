3ef456aca72c78562c70ffb0cb3f3f7779c108e3
NissiJeong <jnissi92@gmail.com>
2024-10-11 14:08:59 +0900

    jpa relation mapping

--- ex2_hello_jpa/src/main/java/jpa_basic_2/Main.java
+++ ex2_hello_jpa/src/main/java/jpa_basic_2/Main.java
@@ -1,5 +1,7 @@
 package jpa_basic_2;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -25,13 +27,20 @@ public class Main {
             em.persist(member);
 
             //영속성 컨텍스트 1차 캐시 말고 직접 db 쿼리를 보고 싶으면
-            //em.flush();
-            //em.clear();
+            em.flush();
+            em.clear();
 
             //조회
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam: "+findTeam.getName());
+            List<Member> members =  findMember.getTeam().getMembers();
+
+            for(Member m : members){
+                System.out.println("m = " + m.getUsername());
+            }
+
+            //Team findTeam = findMember.getTeam();
+            //System.out.println("findTeam: "+findTeam.getName());
+
 
             //수정
             //Team newTeam = em.find(Team.class, 100L);
--- ex2_hello_jpa/src/main/java/jpa_basic_2/Team.java
+++ ex2_hello_jpa/src/main/java/jpa_basic_2/Team.java
@@ -1,9 +1,13 @@
 package jpa_basic_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Team {
@@ -13,6 +17,9 @@ public class Team {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team") //team의 입장에서는 팀이 1 member가 n 이니까 oneToManey 이고, member의 team 속성과 연결이니까 mappedBy 의 값은 team 으로 해주면 됨.
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -28,5 +35,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
